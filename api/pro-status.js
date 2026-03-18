cfa8403c6dc99b47038d0a275d10d19116b8c44f
dentbrandon1122-byte <dentbrandon1122@gmail.com>
2026-03-18 12:55:15 -0400

    Update pro-status.js

--- api/pro-status.js
+++ api/pro-status.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     const pro = await isProUser(userId);
 
-    return res.status(200).json({ pro: !!pro });
+    return res.status(200).json({ pro });
   } catch (error) {
     return res.status(500).json({
       error: error.message || "Server error."
